99e4207f1c62d9828eaf70dc0038f641ca593f5c
Ramzi <ramzi.hannachi1991@gmail.com>
2022-11-14 18:58:18 +0100

    feat: add cache redis

--- src/broker/queues/fibonacci1.queue.ts
+++ src/broker/queues/fibonacci1.queue.ts
@@ -1,25 +1,47 @@
 import amqplib, {Channel, Connection} from 'amqplib/callback_api'
-import {URI_CHANNEL} from "@/config";
+import {URI_CHANNEL, URI_REDIS} from "@/config";
 import Fibonacci from "@/factories";
+import {createClient} from "redis";
 
-const QUEUE_NAME = 'FabSeries2' as const
+const QUEUE_NAME = 'FabSeries1' as const
+
+const client = createClient({ url: URI_REDIS });
 
 export const sendValueInFabQueue1 = (value: number) => {
     amqplib.connect(URI_CHANNEL, (err: Error, connection: Connection) => {
         if (err) process.exit();
-        connection.createChannel((error: Error, channel: Channel) => {
+        connection.createChannel(async (error: Error, channel: Channel) => {
             if (error) {
-                console.log(error);
+                console.error(error);
                 process.exit();
-            }
-            else {
-                const fibonacciInstance = new Fibonacci()
-                const result = fibonacciInstance.calculate(value);
-                channel.assertQueue(QUEUE_NAME, { durable: false });
+            } else {
+                let result
+
+                console.info()
+
+                if (!client.isOpen) {
+                    console.info('===> REDIS connect')
+                    await client.connect();
+                }
+
+                const cache = await client.get(`cache-${QUEUE_NAME}-${value}`)
+
+                if (cache) {
+                    console.info('===> REDIS use cache:', cache)
+                    result = cache
+                } else {
+                    const fibonacciInstance = new Fibonacci()
+                    result = fibonacciInstance.calculate(value);
+                    // cache result into redis
+                    await client.set(`cache-${QUEUE_NAME}-${value}`, result)
+                    console.info('===> REDIS set cache', result)
+                }
+
+                channel.assertQueue(QUEUE_NAME, {durable: false});
                 channel.sendToQueue(QUEUE_NAME, Buffer.from(result.toString()));
-                console.log();
-                console.log(`Send To Queue - ${QUEUE_NAME}`);
-                console.log();
+                console.info();
+                console.info(`===> Send To Queue - ${QUEUE_NAME}`);
+                console.info();
             }
         });
     });
